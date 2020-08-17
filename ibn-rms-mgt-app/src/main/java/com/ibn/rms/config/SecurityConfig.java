0101f3022bd3710e2ca8cbedfb0caca333cca071
CoderLess <417433131@qq.com>
2020-08-17 22:15:28 +0800

    2020年8月17日22:15:18

--- ibn-rms-mgt-app/src/main/java/com/ibn/rms/config/SecurityConfig.java
+++ ibn-rms-mgt-app/src/main/java/com/ibn/rms/config/SecurityConfig.java
@@ -1,12 +1,25 @@
 package com.ibn.rms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibn.rms.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @version 1.0
@@ -19,11 +32,45 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 // 启用Spring Security的Web安全支持
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll()
+        http.authorizeRequests()
+                .and().cors()
+                .and().addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()
+                // 请求成功后的回调
+                .successHandler((request, response, auth) -> response.sendRedirect("/user/index"))
+                // 请求失败后的回调
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    resp.setStatus(401);
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("status", 401);
+                    if (e instanceof LockedException) {
+                        map.put("msg", "账户被锁定，登录失败!");
+                    } else if (e instanceof BadCredentialsException) {
+                        map.put("msg", "账户名或密码输入错误，登录失败!");
+                    } else if (e instanceof DisabledException) {
+                        map.put("msg", "账户被禁用，登录失败!");
+                    } else if (e instanceof AccountExpiredException) {
+                        map.put("msg", "账户已过期，登录失败!");
+                    } else if (e instanceof CredentialsExpiredException) {
+                        map.put("msg", "密码已过期，登录失败!");
+                    } else {
+                        map.put("msg", "登录失败!");
+                    }
+                    ObjectMapper om = new ObjectMapper();
+                    out.write(om.writeValueAsString(map));
+                    out.flush();
+                    out.close();
+                })
                 .and()
-                .formLogin().successForwardUrl("/user/index");
+                // session管理
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     /**
@@ -33,8 +80,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        System.out.println(bCryptPasswordEncoder.encode("123456"));
         return new BCryptPasswordEncoder();
+    }
+    /**
+     * @description: 解决跨域问题
+     * @author：RenBin
+     * @createTime：2020/8/17 21:05
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.applyPermitDefaultValues();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
